5f9180a97a5246f3e8345e843ed3454b1bf2df5e
Pedro Henrique Peixoto Santos <147112228+peagaaa@users.noreply.github.com>
2024-10-10 20:08:54 -0300

    Update telareceita.tsx

--- components/telareceita.tsx
+++ components/telareceita.tsx
@@ -1,90 +1,82 @@
-import { getApi } from '@/services/api'
-import { View, Text, FlatList, StyleSheet, Image, ActivityIndicator } from "react-native";
-import React, { useState, useEffect } from 'react';
-import {useLocalSearchParams} from 'expo-router'
-import PageError from './pageError';
+import { getApi } from "@/services/api";
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  Image,
+  ActivityIndicator,
+} from "react-native";
+import React, { useState, useEffect } from "react";
+import { useLocalSearchParams } from "expo-router";
+import PageError from "./pageError";
+import { useFonts } from "expo-font";
 
+export default function TelaDaReceita() {
+  const { idRecipe, nameRecipe } = useLocalSearchParams();
 
+  const [loaded, error] = useFonts({
+    "Bebas-Neue-Regular": require("../assets/fonts/BebasNeue-Regular.ttf"),
+  });
 
-export default function TelaDaReceita(){
+  const [recipe, setRecipe] = useState(null);
+  const URL = `https://dummyjson.com/recipes/${idRecipe}`;
 
-    const { idRecipe, nameRecipe } = useLocalSearchParams();
+  const burcarDados = async () => {
+    const data = await getApi(URL);
+    setRecipe(data);
+  };
 
-    const [recipe, setRecipe] = useState(null);
-    const URL = `https://dummyjson.com/recipes/${idRecipe}`;
+  useEffect(() => {
+    burcarDados(); // Chama a função ao montar o componente
+  }, []);
 
-    const burcarDados = async () => {
-        const data = await getApi(URL);
-        setRecipe(data);
-    };
+  if (!loaded && !error) {
+    return null;
+  }
 
-    useEffect(() => {
-        burcarDados(); // Chama a função ao montar o componente
-    }, []);
-
-    return(
-        <View
-            style={{flex:1}}
-        >
-            {
-                !recipe ? 
-                (
-                    <PageError/>
-                ): 
-                (
-                    <View
-                        style={styles.body}
-                    >
-                        <Text
-                            style={styles.recipeName}
-                        >{recipe.name}</Text>
-                        <Image
-                            style={styles.imageRecipe}
-                            source={{uri: recipe.image}}
-                        />
-                        <View>
-                            <Text>Informações: </Text>
-                        </View>
-                        <Text>Ingredientes: </Text>
-                        <FlatList
-                            data={recipe.ingredients}
-                            keyExtractor={(ingredient, index) => index.toString()}
-                            renderItem={({item}) => (
-                                <Text>{item}</Text>
-                            )}
-                        />
-                        <Text>Modo de preparo: </Text>
-                        <FlatList
-                            data={recipe.instructions}
-                            keyExtractor={(instruction, index) => index.toString()}
-                            renderItem={({item}) => (
-                                <Text>{item}</Text>
-                            )}
-                        />
-                    </View>
-                )
-            }                                                                                                                                                                                                                                                                                                                                         
+  return (
+    <View style={{ flex: 1 }}>
+      {!recipe ? (
+        <PageError />
+      ) : (
+        <View style={styles.body}>
+          <Text style={styles.recipeName}>{recipe.name}</Text>
+          <Image style={styles.imageRecipe} source={{ uri: recipe.image }} />
+          <View>
+            <Text>Informações: </Text>
+          </View>
+          <Text>Ingredientes: </Text>
+          <FlatList
+            data={recipe.ingredients}
+            keyExtractor={(ingredient, index) => index.toString()}
+            renderItem={({ item }) => <Text>{item}</Text>}
+          />
+          <Text>Modo de preparo: </Text>
+          <FlatList
+            data={recipe.instructions}
+            keyExtractor={(instruction, index) => index.toString()}
+            renderItem={({ item }) => <Text>{item}</Text>}
+          />
         </View>
-    );
+      )}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    body:{
-        
-    },
-    recipeName:{
-        fontFamily:'BebasNeue',
-        fontSize: 40
-    },
-    imageRecipe: {
-        flex: 1,
-        alignItems: 'center',
-        height: 400,
-        width: '90%',
-        marginTop:20,
-        borderRadius: 15,
-        padding:50,
-        
-    },
-
-  });
+  body: {},
+  recipeName: {
+    fontFamily: "Bebas-Neue-Regular",
+    fontSize: 40,
+  },
+  imageRecipe: {
+    flex: 1,
+    alignItems: "center",
+    height: 400,
+    width: "90%",
+    marginTop: 20,
+    borderRadius: 15,
+    padding: 50,
+  },
+});
