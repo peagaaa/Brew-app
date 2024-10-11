f59e33bb63adfd80f7a95f45cfc6fe9aa6d60298
Pedro Henrique Peixoto Santos <147112228+peagaaa@users.noreply.github.com>
2024-10-10 21:40:05 -0300

    Create icon.tsx

---
+++ components/icon.tsx
@@ -0,0 +1,38 @@
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import React from "react";
+import Icon from "react-native-vector-icons/AntDesign";
+import { useState } from "react";
+
+export default function Icone() {
+
+    const fullHeart = 'heart';
+    const empytHeart = 'hearto';
+
+    const [ changeIcon, setChangeIcon ] = useState(empytHeart);
+
+    const changeHeartIcon = () => {
+        if(changeIcon == empytHeart){
+            setChangeIcon(fullHeart);
+        }else{
+            setChangeIcon(empytHeart);
+        }
+    }
+
+
+  return (
+    <TouchableOpacity style={styles.iconWrapper} onPress={changeHeartIcon}>
+      <Icon name={changeIcon} size={40} color="red" />
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+    iconWrapper: {
+    position: "absolute",
+    zIndex: 1,
+    right: 25,
+    top: 25,
+  },
+});
+
+// picker item pesquisar 
