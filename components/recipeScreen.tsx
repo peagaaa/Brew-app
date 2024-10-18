c9b8a3cbe2b0afb7b83068044f6f161a381a9127
Pedro Henrique Peixoto Santos <147112228+peagaaa@users.noreply.github.com>
2024-10-17 21:00:40 -0300

    Update recipeScreen.tsx

--- components/recipeScreen.tsx
+++ components/recipeScreen.tsx
@@ -1,110 +1,88 @@
 import {
   View,
   Text,
-  SafeAreaView,
   StyleSheet,
   FlatList,
   Image,
-  Button,
-  TouchableOpacity
+  TouchableOpacity,
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import { Link } from "expo-router";
 import Icone from "./icon";
-import AntDesign from '@expo/vector-icons/AntDesign';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon from "react-native-vector-icons/FontAwesome";
+import useCustomFonts from "@/hooks/useFonts";
+import { fetchRecipes } from '../services/fecthrecipes'; // Importando a função
 
 const RecipeScreen = () => {
+  const fontsLoaded = useCustomFonts();
 
   const limitRecipe = 2;
-  const [ skipRecipe, setSkipRecipe ] = useState(0)
-
+  const [skipRecipe, setSkipRecipe] = useState(0);
   const URL = `https://dummyjson.com/recipes?limit=${limitRecipe}&skip=${skipRecipe}`;
-
-  // ?limit=5 & skip=4
-
+  
   const [recipes, setReceitas] = useState([]);
-  const [error, setError] = useState(true);
+  const [error, setError] = useState(null);
   const [loading, setLoading] = useState(null);
 
   useEffect(() => {
-    const getApi = async () => {
-      try {
-        const response = await fetch(URL);
-        if (!response.ok) {
-          throw new Error("ERRO: " + response.status);
-        }
-        const data = await response.json();
-        // console.log(data);
-        setReceitas(data.recipes);
-      } catch (err) {
-        setError(err.message);
-      } /*finally {
-          setLoading(false);
-        }*/
-    };
-    getApi();
+    fetchRecipes(URL, setReceitas, setError);
   }, [skipRecipe]);
 
   return (
     <>
-    <FlatList
-      data={recipes}
-      keyExtractor={(item) => item.id.toString()}
-      renderItem={({ item }) => (
-        <View
-          style={{
-            padding: 16,
-            alignItems: "center",
-            position:'relative'
-          }}
-        >
-          <Image
-            source={{ uri: item.image }}
-            style={{ width: "100%", height: 200 }}
-          />
-          <Link
-            //?id=${id}&nome=${nome}
-            href={`./recipes/?idRecipe=${item.id}`}
-            // href={`./recipes/`}
-            style={styles.desciptionRecipies}
+      <FlatList
+        data={recipes}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View
+            style={{
+              padding: 16,
+              alignItems: "center",
+              position: "relative",
+            }}
           >
-            <View style={styles.desciptionRecipies}>
-              <Text>{item.name}</Text>
-              <Text>Origem: {item.cuisine}</Text>
-              <Text>
-                Tempo da receita: {item.prepTimeMinutes + item.cookTimeMinutes}{" "}
-                Minutes
-              </Text>
-              <Text>Nota: {item.rating}</Text>
-            </View>
-          </Link>
-        </View>
-      )}
-    /> 
-    <TouchableOpacity 
-      style={styles.containerButton}
-      onPress={() => {
-        if(skipRecipe <= 50){
-          setSkipRecipe(skipRecipe + 2)
-        }else{
-          setSkipRecipe(skipRecipe * 0)
-        }
-      }}
-    >
-    <Icon
-      name='plus'
-      color='red'
-      size={50}
-      style={styles.iconPlusRecipes}
-    />
-    </TouchableOpacity>
-</>
+            <Image
+              source={{ uri: item.image }}
+              style={{ width: "100%", height: 200 }}
+            />
+            <Icone style={styles.iconHeart} />
+            <Link
+              href={`./recipes/?idRecipe=${item.id}`}
+              style={styles.desciptionRecipies}
+            >
+              <View style={styles.desciptionRecipies}>
+                <Text style={styles.textRecipeDescription}>{item.name}</Text>
+                <Text style={styles.textRecipeDescription}>
+                  Origem: {item.cuisine}
+                </Text>
+                <Text style={styles.textRecipeDescription}>
+                  Tempo da receita: {item.prepTimeMinutes + item.cookTimeMinutes} Minutes
+                </Text>
+                <Text style={styles.textRecipeDescription}>
+                  Nota: {item.rating}
+                </Text>
+              </View>
+            </Link>
+          </View>
+        )}
+      />
+      <TouchableOpacity
+        style={styles.containerButton}
+        onPress={() => {
+          if (skipRecipe <= 50) {
+            setSkipRecipe(skipRecipe + 2);
+          } else {
+            setSkipRecipe(0);
+          }
+        }}
+      >
+        <Icon name="plus" color="red" size={50} />
+      </TouchableOpacity>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
-  body: {},
   desciptionRecipies: {
     flex: 1,
     width: "99%",
@@ -113,26 +91,25 @@ const styles = StyleSheet.create({
     borderBottomRightRadius: 12,
     borderBottomLeftRadius: 12,
   },
-  icon: {
+  iconHeart: {
     position: "absolute",
-    right: 25,
-    top: 15,
+    right: 30,
+    top: 30,
   },
-  containerButton:{
-    backgroundColor: 'white',
+  containerButton: {
+    backgroundColor: "white",
     height: 75,
     width: 75,
     borderRadius: 50,
-    alignItems:'center',
-    justifyContent:'center',
-    position:'absolute',
+    alignItems: "center",
+    justifyContent: "center",
+    position: "absolute",
     bottom: 100,
-    right: 25
+    right: 25,
   },
-  iconPlusRecipes:{
-
-  }
+  textRecipeDescription: {
+    fontFamily: "Mont-Serrat",
+  },
 });
 
 export default RecipeScreen;
-// link
